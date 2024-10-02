f5f267672f879bb5960e06a6476055d31b6bfb14
wara-web <okothensisb@gmail.com>
2024-10-02 11:56:40 +0300

    task manager frontend

--- task_management/urls.py
+++ task_management/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include  # Ensure 'include' is imported here
+from django.urls import path, include  
 from . import views  # Import your views if needed
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('tasks.urls')),  # Ensure 'tasks.urls' is the correct module
-    path('', views.home, name='home'),  # Optional: add a home path
-]
+    path('api/', include('tasks.urls')), 
+    path('', views.home, name='home'),  
+   ]
